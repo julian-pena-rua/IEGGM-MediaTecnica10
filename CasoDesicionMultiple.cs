5542dfa3b786ddd7b9fb5efa467f116f6b9cf2a0
Julian Peña Rúa <julian.pena@hotmail.com>
2024-05-29 15:58:44 -0500

    Create CasoDesicionMultiple.cs

---
+++ CasoDesicionMultiple.cs
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                Ejercicio estructura de decisión múltiple
+Se desea un programa que ayude diciendo 
+las preferencias según el género:
+- Si es un hombre menor de edad sus preferencias son:
+    Figuras de acción, Caricaturas, Series TV
+- Si es un hombre mayor de edad sus preferencias son:
+    cine, picnic y filantropía
+- Si es una mujer menor de edad sus preferencias son:
+    Muñecas, Maquillaje, Series TV
+- Si es una mujer mayor de edad sus preferencias son:
+    Maquillaje, Series TV, Cine, filantropía
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+    
+  }
+}
