620050d1aafe61c0721d166483321155dcbaf4ef
Julian Peña Rúa <julian.pena@hotmail.com>
2024-05-29 14:41:30 -0500

    Crear MiniCalculadora.cs
    Se crea ejercicio para validaciones múltiples o estructuras de decisión múltiple.

---
+++ MiniCalculadora.cs
@@ -0,0 +1,61 @@
+/***********************************************************************
+Autor:          Julián A. Peña
+Fecha:          29-05-2024 
+github:         https://github.com/julian-pena-rua
+Descripción:    
+
+
+    ************
+    App con Menú
+    ************
+    Se solicita crear una minicalculadora que realice:
+    - Suma
+    - Resta
+    - Multiplicación
+    - División
+    Debe terminar la ejecución al usuario elegir opción "Salir"
+***********************************************************************/
+using System;
+using System.Threading;
+
+class Program {
+    static void Main(string[] args) {		
+    // 1 Pantalla bienvenida (imagen ASCII)
+    Console.WriteLine("                                                    			");
+    Console.WriteLine("                       :#*-.                        			");
+    Console.WriteLine("                     :%%%%##%%%%*-.                 			");  			
+    Console.WriteLine("                   .#*--=-=+#%%%%##%%%#+:           			");
+    Console.WriteLine("                  .##-+--===-=+*%%%%%##%%%+:        			");
+    Console.WriteLine("                .*#**#%%*=-==:===-:.-+#%%%%+        			");
+    Console.WriteLine("               .*####***#%%#=-=-:=-::..:#%*.        			");
+    Console.WriteLine("              .*#****###*++*%%#+-=::-:.-%*.         			");
+    Console.WriteLine("            .*#%#%**##%##*******+*#%#+%#:           			");
+    Console.WriteLine("           .*####**##%**##%*#*****+++*#-            			");
+    Console.WriteLine("          *****++##%***##*###*+###*+#=              			");
+    Console.WriteLine("         ****#*+#****#*%+**##+#***+#=               			");
+    Console.WriteLine("        +**+*++*###****+#*#*=+##%=*+                			");
+    Console.WriteLine("       :+#*++**+%#*=+#*%==+*%++*.                   			");
+    Console.WriteLine("         .=##+-+##+***+=+#%#-+#.                    			");
+    Console.WriteLine("              .-*#+==--*%%*=#:                      			");
+    Console.WriteLine("                 .-*#*==*+-#:                       			");
+    Console.WriteLine("                    .-*#*=+:    					");           
+    Console.WriteLine("                        :=-     					");
+    Console.WriteLine("       Bienvenido a la MiniCalculadora ");
+    Thread.Sleep(3000); // 3 segundos = 3000 milisegundos
+    Console.Clear();
+    // 2 Mostrar menú de opciones
+    Console.WriteLine("Menú de opciones");
+    Console.WriteLine("Seleccionar la opción deseada:");
+    Console.WriteLine("1. Suma");
+    Console.WriteLine("2. Resta");
+    Console.WriteLine("3. Multiplicación");
+    Console.WriteLine("4. Divisón");
+    Console.WriteLine("5. Salir");
+    Console.Write("Digitar numero deseado: ");
+    int opcion = Int32.Parse(Console.ReadLine());
+    
+    // 3 capturar opción elegida por usuario
+    // 4 Validar opción elegida
+    // 5 Mostrar resultado o finalizar ejecución
+    }
+}
