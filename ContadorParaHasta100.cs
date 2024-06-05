1540645f8ff19af6ec8b415c741ae2a529419802
Julian Peña Rúa <julian.pena@hotmail.com>
2024-06-05 13:35:43 -0500

    Create ContadorParaHasta100.cs
    programa aprendizaje estructuras repetitivas o bucles.

---
+++ ContadorParaHasta100.cs
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Autor: Julián A. Peña
+Fecha: 2024-06-05
+Descripción: Programa aprendizaje ciclos o bucles
+             Contador hasta 100
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+    for (int i = 0; i <= 100; i++) 
+    {
+        Console.WriteLine("El valor actual es: " + i);
+    }
+  }
+}
