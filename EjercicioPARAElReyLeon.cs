9d16298ef94812cbafa2056bf99e19e0d4c39e48
Julian Peña Rúa <julian.pena@hotmail.com>
2024-06-05 14:47:15 -0500

    Create EjercicioPARAElReyLeon.cs
    Ejercicio

---
+++ EjercicioPARAElReyLeon.cs
@@ -0,0 +1,20 @@
+/******************************************************************************
+Autor: Julián A. Peña
+Fecha: 2024-06-05
+Descripción: El Rey León: 
+    Las manadas de la Roca del Rey están compitiendo 
+    por el control de las Tierras del Reino. 
+    Pide al usuario la fuerza de al menos 5 
+    clanes diferentes y usa estructuras "si" 
+    para determinar clasificación de fuerza.
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+    // variables
+    // estructura loop
+        // solicitud de fuerza
+        // validación y clasificación
+    // fin
+  }
+}
