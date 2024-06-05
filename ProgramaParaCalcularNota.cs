b527bdc12d947961a9b3f590d6eaedd79931d5ac
Julian Peña Rúa <julian.pena@hotmail.com>
2024-06-05 13:44:42 -0500

    Create ProgramaParaCalcularNota.cs
    Programa aprendizaje estructura bucles PARA

---
+++ ProgramaParaCalcularNota.cs
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Autor: Julián A. Peña
+Fecha: 2024-06-05
+Descripción: Programa aprendizaje ciclos o bucles
+             Calcular promedio para nota.
+             El docente brinda 4 notas y debe calcular el promedio.
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+    // variables
+    int nota;
+    int promedio;
+    
+    // bienvenida a la app
+    Console.WriteLine("Bienvenido a miniApp promedio notas.");
+    
+    // solicitud notas
+    for (int i = 0; i <= 1; i++) 
+    {
+        Console.WriteLine("Docente, favor ingresar la nota " + i + ", luego presionar enter");
+    }
+    
+    // mostrar promedio
+  }
+}
