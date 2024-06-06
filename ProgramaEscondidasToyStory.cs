dcdb8cc2ef312f93747a498f7f2e9691574c3138
Julian Peña Rúa <julian.pena@hotmail.com>
2024-06-06 13:56:55 -0500

    Create ProgramaEscondidasToyStory.cs
    Creación ejercicio

---
+++ ProgramaEscondidasToyStory.cs
@@ -0,0 +1,26 @@
+/******************************************************************************
+Autor:          Julián A. Peña
+Fecha:          06-06-2024
+Descripción:    Toy Story
+     Los juguetes de Andy están 
+     jugando a las escondidas. Pide al 
+     usuario las ubicaciones donde buscar 
+     y usa estructuras "si" para determinar 
+     si los juguetes están en la ubicación 
+     correcta o no. Reportar la cantidad 
+     total de juguetes que fueron encontrados 
+     al finalizar 4 rondas.
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() {
+    // variables
+    // estructura repetitiva o ciclicla
+        // preguntar ubicaciones al usuario
+        // validar ubicaciones con estructura IF
+            // acumular juguetes encontrados
+    //mostrar juguetes encontrados
+    // fin
+  }
+}
