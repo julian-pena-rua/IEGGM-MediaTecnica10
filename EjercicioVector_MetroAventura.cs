152cdd5d1e0eaf0162d878d7c0c3a251a63b78d6
Julian Peña Rúa <julian.pena@hotmail.com>
2024-07-11 13:49:18 -0500

    Update EjercicioVector_MetroAventura.cs
    Se añade ASCII

--- EjercicioVector_MetroAventura.cs
+++ EjercicioVector_MetroAventura.cs
@@ -38,5 +38,27 @@ class MetroAventura {
     
     string[] jugador = new string[2]; 
     
-  }
+    // pantalla bienvenida del juego
+    Console.WriteLine("0l:cccccccccccccccccccccccccccccccccclxK");
+    Console.WriteLine("o.*,**..****************......**.....**o");
+    Console.WriteLine("l*xKOxoc;,*****************************o");
+    Console.WriteLine("l*xNWMWNX0kdl:,**.................****,o");
+    Console.WriteLine("l.cxk0KXNWWWWX0kdoc;,*............****,o");
+    Console.WriteLine("l..*,;:loxO0KXXNNNX0Okxoc:,.....*******o");
+    Console.WriteLine("l........*,;:ldxO0XXXXXXK0kc*.*********o");
+    Console.WriteLine("l............*cdOKXXXXXKK0Ol*.******..*o");
+    Console.WriteLine("l........;oxOKXNNNXK00Okdl:*..........*o");
+    Console.WriteLine("l ......,kWWWWWNNKOdlc,*...............o");
+    Console.WriteLine("l .......lO0KXNNNNX0Oxoc;,.............l");
+    Console.WriteLine("l  .......*,:ldkO0XXXXXKKOx:...........l");
+    Console.WriteLine("l  .... .. ..*:oO0XXXXXKK0Oc...........l");
+    Console.WriteLine("l    ...,codOKXNNNXK00Okdo:*...........l");
+    Console.WriteLine("l..;cdkKXWWWNXK0Okdlc:,*...............l");
+    Console.WriteLine("c*xNWMWNXK0Oxoc;,*....  ...............l");
+    Console.WriteLine("c*kXK0kdl:;*...  ......................l");
+    Console.WriteLine("l           METRO DE MEDELLIN          l");
+    Console.WriteLine("Kdc:;;;::::ccccccccccccccccccccccccccco0");
+    Console.WriteLine("WWNXXXXNNNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWM");
+    
+}
 }
