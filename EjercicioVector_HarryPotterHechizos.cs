ddee81b2522830129fe30b61d130b3e37d523c85
Julian Peña Rúa <julian.pena@hotmail.com>
2024-07-11 15:14:53 -0500

    Create EjercicioVector_HarryPotterHechizos.cs

---
+++ EjercicioVector_HarryPotterHechizos.cs
@@ -0,0 +1,26 @@
+/*
+
+
+
+
+Harry Potter necesita un programa 
+en C# para listar sus hechizos. 
+Completa el espacio en blanco 
+para mostrar los nombres de los 
+hechizos en la consola:
+*/
+ 
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] hechizos = { "Expecto Patronum", "Wingardium Leviosa", "Expelliarmus" };
+        __________________
+    	__________________
+       	__________________
+    	__________________
+    	__________________
+    }
+}
